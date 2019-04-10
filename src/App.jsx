29ee77fe488049f3470e87a9508182f2f6132eed
dominicho97 <dominic.ho97@gmail.com>
2019-04-10 14:26:14 +0200

    added 2nd and 3rd form, deleted submit from 2nd form

--- src/App.jsx
+++ src/App.jsx
@@ -5,54 +5,73 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "small pasta" };
+    this.state = { value: "Small pasta", sauce: "Mushroom sauce" };
 
     this.handleChange = this.handleChange.bind(this);
-    //this.handleChangeSauce = this.handleChangeSauce.bind(this);
+    this.handleChangeSauce = this.handleChangeSauce.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  /*handleChangeSauce(event) {
-    this.setState({ value2: event.target.value });
-  }*/
 
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
+  handleChangeSauce(event) {
+    this.setState({ value: event.target.value });
+  }
 
   handleSubmit(event) {
-    alert("Your order is: " + this.state.value);
+    alert("Your order is: " + this.state.value + " with " + this.state.sauce);
     event.preventDefault();
   }
+  /* handleSubmitSauce(event) {
+    alert("Your order is: " + this.state.sauce);
+    event.preventDefault();
+  }*/
 
   render() {
     return (
       <div>
-        <div className="pasta-size">
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Choose your pasta size:
-              <select value={this.state.value} onChange={this.handleChange}>
-                <option value="Small pasta">Small</option>
-                <option value="Medium pasta">Medium</option>
-                <option value="Large pasta">Large</option>
-                <option value="Extra large pasta">Extra large</option>
-              </select>
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
+        <div>
+          <div>
+            <div className="pasta-size">
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  Choose your pasta size:
+                  <select value={this.state.value} onChange={this.handleChange}>
+                    <option value="Small pasta">Small</option>
+                    <option value="Medium pasta">Medium</option>
+                    <option value="Large pasta">Large</option>
+                    <option value="Extra large pasta">Extra large</option>
+                  </select>
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+            <div className="pasta-sauce">
+              <form>
+                <label>
+                  Choose your pasta sauce:
+                  <select>
+                    <option value="Tomato sauce">Tomato sauce</option>
+                    <option value="Mushroom sauce">Mushroom sauce</option>
+                    <option value="Pesto sauce">Pesto sauce</option>
+                  </select>
+                </label>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="pasta-sauce">
+        <div className="pasta-topping">
           <form onSubmit={this.handleSubmit}>
             <label>
-              Choose your pasta sauce:
+              Choose your pasta topping:
               <select
                 value={this.state.value}
                 //onChange={this.handleChangeSauce}
               >
-                <option value="Tomato sauce">Tomato sauce</option>
-                <option value="Mushroom sauce">Mushroom sauce</option>
-                <option value="Pesto sauce">Pesto sauce</option>
+                <option value="Fish ">Fish</option>
+                <option value="Meat">Meat</option>
+                <option value="Vegetables">vegetables</option>
               </select>
             </label>
             <input type="submit" value="Submit" />
